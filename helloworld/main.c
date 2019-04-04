f1eab384db2922b27109a47c942ebb1787979a7a
Reg Orton <reg@brck.com>
2019-04-04 21:28:13 +0300

    LED blink (untested)

--- helloworld/main.c
+++ helloworld/main.c
@@ -1,6 +1,32 @@
 #include <xc.h>
 
+#define _XTAL_FREQ 4000000
+
+#pragma config WDTE=OFF, PWRTE=OFF, MCLRE=ON, CP=OFF, CPD=OFF, BOREN=OFF, IESO=OFF, FCMEN=OFF
+
+//Define the LED port states.
+#define LED	    PORTBbits.RB7
+#define LED_TRIS    TRISBbits.TRISB7
+
 int main()
 {
-    NOP();
+    TRISA = 0xFF; //make everything DI 
+    TRISB = 0xFF; //make everything DI 
+    TRISC = 0xFF; //make everything DI 
+
+ //  ANSEL = 0x00; //disable analog inputs
+ //  ANSELH = 0x00;
+
+    //TRISBbits.TRISB7 = 0; //make the LED pin an output //debug changed to tomorrow.
+
+    LED_TRIS = 0; //Make the LED pin an output
+
+    while(1)
+    {
+        LED = 1;            //Flash the LED
+        __delay_ms(500);
+        LED = 0;
+        __delay_ms(500);
+     }
+    return 0;
 }
